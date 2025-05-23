f0d7f226674cb35a4c36804697f00decb0373a6a
jayce <15202844993@163.com>
2025-05-23 16:53:53 +0800

    Affirmation

--- projects/Affirmations/src/main/java/com/example/affirmations/MainActivity.kt
+++ projects/Affirmations/src/main/java/com/example/affirmations/MainActivity.kt
@@ -20,10 +20,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
@@ -34,6 +40,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -60,15 +67,53 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Preview
 @Composable
-fun AffirmationsApp() {
-    AffirmationList(
-        affirmationList = Datasource().loadAffirmations(),
+private fun AffirmationCardPreview() {
+    AffirmationCard(
+        Affirmation(
+            R.string.affirmation1,
+            R.drawable.image1
+        )
     )
 }
 
 @Composable
-fun AffirmationList(affirmationList: List<Affirmation>, modifier: Modifier = Modifier) {
+fun AffirmationsApp() {
+//    val layoutDirection = LocalLayoutDirection.current
+//    Surface(
+//        modifier = Modifier
+//            .fillMaxSize()
+//            .statusBarsPadding()
+//            .padding(
+//                start = WindowInsets.safeDrawing.asPaddingValues().calculateStartPadding(layoutDirection),
+//                end = WindowInsets.safeDrawing.asPaddingValues().calculateEndPadding(layoutDirection)
+//            )
+//    ) {
+//        AffirmationList(
+//            affirmationList = Datasource().loadAffirmations()
+//        )
+//    }
+
+//    AffirmationList(
+//        affirmationList = Datasource().loadAffirmations()
+//    )
+
+    LazyColumn() {
+        items(Datasource().loadAffirmations()) { affirmation ->
+            AffirmationCard(
+                affirmation = affirmation,
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun AffirmationList(
+    affirmationList: List<Affirmation>,
+    modifier: Modifier = Modifier
+) {
     LazyColumn(modifier = modifier) {
         items(affirmationList) { affirmation ->
             AffirmationCard(
@@ -80,7 +125,10 @@ fun AffirmationList(affirmationList: List<Affirmation>, modifier: Modifier = Mod
 }
 
 @Composable
-fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier) {
+fun AffirmationCard(
+    affirmation: Affirmation,
+    modifier: Modifier = Modifier
+) {
     Card(modifier = modifier) {
         Column {
             Image(
@@ -100,8 +148,48 @@ fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier) {
     }
 }
 
-@Preview
-@Composable
-private fun AffirmationCardPreview() {
-    AffirmationCard(Affirmation(R.string.affirmation1, R.drawable.image1))
-}
+//@Composable
+//fun AffirmationsApp() {
+//    AffirmationList(
+//        affirmationList = Datasource().loadAffirmations(),
+//    )
+//}
+//
+//@Composable
+//fun AffirmationList(affirmationList: List<Affirmation>, modifier: Modifier = Modifier) {
+//    LazyColumn(modifier = modifier) {
+//        items(affirmationList) { affirmation ->
+//            AffirmationCard(
+//                affirmation = affirmation,
+//                modifier = Modifier.padding(8.dp)
+//            )
+//        }
+//    }
+//}
+//
+//@Composable
+//fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier) {
+//    Card(modifier = modifier) {
+//        Column {
+//            Image(
+//                painter = painterResource(affirmation.imageResourceId),
+//                contentDescription = stringResource(affirmation.stringResourceId),
+//                modifier = Modifier
+//                    .fillMaxWidth()
+//                    .height(194.dp),
+//                contentScale = ContentScale.Crop
+//            )
+//            Text(
+//                text = LocalContext.current.getString(affirmation.stringResourceId),
+//                modifier = Modifier.padding(16.dp),
+//                style = MaterialTheme.typography.headlineSmall
+//            )
+//        }
+//    }
+//}
+//
+//@Preview
+//@Composable
+//private fun AffirmationCardPreview() {
+//    AffirmationCard(Affirmation(R.string.affirmation1, R.drawable.image1))
+//}
